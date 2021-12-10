55654387aff175ce1fb1bc4bcb5a1c871dffca9b
Samuel Beaulieu <samuel+14401380@puppet.com>
2021-12-10 12:32:23 -0600

    fix simplecov with jruby, add a .rubocop.yml config file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# require 'simplecov'
-# SimpleCov.start do
-#   add_filter '/spec/'
-# end
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
 require 'helpers'
 require 'rspec'
 require 'vmpooler'
